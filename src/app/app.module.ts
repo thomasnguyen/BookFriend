815f6599b6500ff40b507c5c4fd809133dee2a1a
Thomas Nguyen <imtoshiru@gmail.com>
2019-09-30 21:21:22 -0700

    Created 3 new methods in auth service to login/logout and get auth state of user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { auth } from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
+	user$: Observable<User>;
 
-  constructor() { }
+
+	constructor(
+		private afAuth: AngularFireAuth,
+		private afs: AngularFirestore,
+		private router: Router,
+  // tslint:disable-next-line: indent
+  ) {
+	this.user$ = this.afAuth.authState.pipe(
+		switchMap(user => {
+        if (user) {
+          return this.afs.doc<User>('users/$user.uid').valueChanges();
+        } else {
+          return of(null);
+        }
+		  })
+	  );
+  }
+
+  async googleSignIn() {
+    const provider = new auth.GoogleAuthProvider();
+    const credential = await this.afAuth.auth.signInWithPopup(provider);
+
+    return this.updateUserData(credential.user);
+  }
+
+  async signOut() {
+    await this.afAuth.auth.signOut();
+    return this.router.navigate(['/']);
+  }
+
+  private updateUserData(user) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      avatar: user.avatar,
+    };
+
+    return userRef.set(data, { merge: true });
+  }
 }
