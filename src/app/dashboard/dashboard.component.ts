18d712b9d7587a2664e3854c51584b72935410b1
Thomas Nguyen <imtoshiru@gmail.com>
2019-10-06 00:33:16 -0700

    Added small bells and whistles

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageMap } from '@ngx-pwa/local-storage';
 import { User } from '../models/user.model';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,10 +11,12 @@ import { User } from '../models/user.model';
 export class DashboardComponent implements OnInit {
   currentUser: User;
   constructor(
+    private title: Title,
     private storage: StorageMap,
   ) { }
 
   ngOnInit() {
+    this.title.setTitle('BookFriends - Dashboard');
     this.storage.get('currentUser').subscribe((user: User) => {
       // console.log(user);
       this.currentUser = user;
