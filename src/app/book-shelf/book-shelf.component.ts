25ef6a8e83d2f5d864de37f4ad607fd9c91d858c
thomasnguyen <imtoshiru@gmail.com>
2019-11-19 17:12:59 -0800

    Made call to get books

--- src/app/book-shelf/book-shelf.component.ts
+++ src/app/book-shelf/book-shelf.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { StorageMap } from "@ngx-pwa/local-storage";
+import { BookShelfService } from "../services/bookshelf.service";
+import { User } from "../models/user.model";
 
 @Component({
-  selector: 'app-book-shelf',
-  templateUrl: './book-shelf.component.html',
-  styleUrls: ['./book-shelf.component.scss'],
+  selector: "app-book-shelf",
+  templateUrl: "./book-shelf.component.html",
+  styleUrls: ["./book-shelf.component.scss"]
 })
 export class BookShelfComponent implements OnInit {
+  currentUser: User;
+  bookshelf = [];
+  constructor(
+    private storage: StorageMap,
+    private bookShelfSvc: BookShelfService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.storage.get("currentUser").subscribe((user: User) => {
+      // console.log(user);
+      this.currentUser = user;
+      this.bookShelfSvc.getBookshelf(this.currentUser.uid).subscribe(res => {
+        this.bookshelf = this.bookshelf;
+      });
+    });
+  }
 }
