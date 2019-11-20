c10cb435ffec4f4064e0f893b9ded61347e7de45
thomasnguyen <imtoshiru@gmail.com>
2019-11-19 17:01:41 -0800

    skeleton html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,62 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SplashComponent } from './splash/splash.component';
-import { AuthModule } from './auth/auth.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { StorageModule } from '@ngx-pwa/local-storage';
-import { NavComponent } from './nav/nav.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { SplashComponent } from "./splash/splash.component";
+import { AuthModule } from "./auth/auth.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { StorageModule } from "@ngx-pwa/local-storage";
+import { NavComponent } from "./nav/nav.component";
+import { BookShelfComponent } from "./book-shelf/book-shelf.component";
 
 const CREDENTIALS = {
-	apiKey: 'AIzaSyAUp4HpskqbGMZuXmh2y4G_iASr6bDuEVU',
-	authDomain: 'bookfriend-app.firebaseapp.com',
-	databaseURL: 'https://bookfriend-app.firebaseio.com',
-	projectId: 'bookfriend-app',
-	storageBucket: '',
-	messagingSenderId: '1055995931918',
-	appId: '1:1055995931918:web:894c9fda6f053b92253b5f',
-	measurementId: 'G-CTVLY5XWZL',
+  apiKey: "AIzaSyAUp4HpskqbGMZuXmh2y4G_iASr6bDuEVU",
+  authDomain: "bookfriend-app.firebaseapp.com",
+  databaseURL: "https://bookfriend-app.firebaseio.com",
+  projectId: "bookfriend-app",
+  storageBucket: "",
+  messagingSenderId: "1055995931918",
+  appId: "1:1055995931918:web:894c9fda6f053b92253b5f",
+  measurementId: "G-CTVLY5XWZL"
 };
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		SplashComponent,
-		DashboardComponent,
-		NavComponent],
-	entryComponents: [],
-	imports: [
-		BrowserModule,
-		IonicModule.forRoot(),
-		AppRoutingModule,
-		AngularFireModule.initializeApp(CREDENTIALS),
-		AngularFireAuthModule,
-		AngularFirestoreModule,
-		StorageModule.forRoot({ IDBNoWrap: true }),
-		
-		// AuthModule,
-	],
-	providers: [
-		StatusBar,
-		SplashScreen,
-		{
-			provide: RouteReuseStrategy,
-			useClass: IonicRouteStrategy
-		}],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    SplashComponent,
+    DashboardComponent,
+    BookShelfComponent,
+    NavComponent
+  ],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(CREDENTIALS),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    StorageModule.forRoot({ IDBNoWrap: true })
+
+    // AuthModule,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
- }
+export class AppModule {}
